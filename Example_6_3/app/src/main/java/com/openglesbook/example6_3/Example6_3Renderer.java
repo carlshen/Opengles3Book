250451ef7903bd44a36e794794126f4ad55341fd
xiangfeng shen <xiangfeng.shen@outlook.com>
2022-10-15 17:59:28 +0800

    update Example_6_3 project

--- Example_6_3/app/src/main/java/com/openglesbook/example6_3/Example6_3Renderer.java
+++ Example_6_3/app/src/main/java/com/openglesbook/example6_3/Example6_3Renderer.java
@@ -38,18 +38,18 @@
 
 package com.openglesbook.example6_3;
 
-import android.content.Context;
-import android.opengl.GLES30;
-import android.opengl.GLSurfaceView;
-
-import com.openglesbook.common.ESShader;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+
+import com.openglesbook.common.ESShader;
+
+import android.content.Context;
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
 
 public class Example6_3Renderer implements GLSurfaceView.Renderer
 {
